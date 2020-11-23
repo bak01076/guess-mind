9928b0c0c0819acdcbb48d7e8a796ad135faa733
Jongok Yoon <bak01076@gmail.com>
2020-11-23 10:32:20 +0100

    changed

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-import { join } from 'path';
-import express from 'express';
-import socketIO from 'socket.io';
-import logger from 'morgan';
-import socketController from './socketController';
-import events from './events';
+import { join } from "path";
+import express from "express";
+import socketIO from "socket.io";
+import logger from "morgan";
+import socketController from "./socketController";
+import events from "./events";
 
 const PORT = 4000;
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', join(__dirname, 'views'));
-app.use(logger('dev'));
-app.use(express.static(join(__dirname, 'static')));
-app.get('/', (req, res) =>
-  res.render('home', { events: JSON.stringify(events) })
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) =>
+  res.render("home", { events: JSON.stringify(events) })
 );
 
 const handleListening = () =>
@@ -22,4 +22,4 @@ const server = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(server);
 
-io.on('connection', (socket) => socketController(socket));
+io.on("connection", (socket) => socketController(socket));
